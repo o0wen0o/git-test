104b177fe27001c9b535dc66f2b27afa00e6b437
o0wen0o <o0wen0o@hotmail.com>
2022-06-17 21:15:33 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,20 @@
+package com.atguigu.git;
+
+/**
+ * @author o0wen0o
+ * @create 2022-06-17 9:04 PM
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+        System.out.println("master test!");
+        System.out.println("hot-fix test!");
+        System.out.println("push test!");
+        System.out.println("pull test!");
+        System.out.println("gitee test!");
+        System.out.println("gitee test2!");
+    }
+}
